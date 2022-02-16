44ce26374d24b2f4ab3496a166da6ca92f6eabfa
navtej822 <72632038+navtej822@users.noreply.github.com>
2022-02-16 08:17:46 -0600

    latest

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ app.use("/",contactRoute);
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"));
-    app.get("*",(req,res)=>
+    app.get("/",(req,res)=>
         res.sendFile(path.resolve(__dirname,"client","build", "index.html"))
     );
 }
 
-const port = process.env.PORT || 8000;
-app.listen(port, console.log(`server listening to port 8000 only`));
+const port = process.env.PORT || 6000;
+app.listen(port, console.log(`server listening to port 6000 only`));
