c253a57b3b00833fc4f37877b144eed196649821
navtej822 <72632038+navtej822@users.noreply.github.com>
2022-02-16 08:00:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -21,5 +21,5 @@ if(process.env.NODE_ENV === "production"){
     );
 }
 
-const port = process.env.PORT || 6000;
-app.listen(port, console.log(`server listening to port 6000 only`));
+const port = process.env.PORT || 5000;
+app.listen(port, console.log(`server listening to port 5000 only`));
